18801050ec927551b4958a42bb8c57609c05c024
Etienne Collin <collin.etienne.contact@gmail.com>
2024-02-12 22:03:11 -0500

    Started implementing command execution

--- src/shell.c
+++ src/shell.c
@@ -15,7 +15,7 @@
  * Creates a new process
  * @return the status of the child
  */
-int execute_command(const struct command *cmd, enum op previous_op) {
+int execute_command(const struct command *cmd, enum op previous_op, int previous_result) {
     pid_t pid = fork();
     int status = 0;
     int pipefd[2];
@@ -25,7 +25,6 @@ int execute_command(const struct command *cmd, enum op previous_op) {
         perror("Fork failed");
         return EXECUTION_FAILED;
     } else if (pid == 0) { // child process
-
         // Set the input and output
         if (previous_op == OP_PIPE) {
             dup2(pipefd[0], STDIN_FILENO);
@@ -34,8 +33,16 @@ int execute_command(const struct command *cmd, enum op previous_op) {
             dup2(pipefd[1], STDOUT_FILENO);
         }
 
-        int result = execvp(cmd->args[0], cmd->args); // returns -1 if the command failed
-        if (result == EXECUTION_FAILED) {
+        if (previous_op == OP_AND && previous_result != 0) {
+            return EXECUTION_SUCCESS;
+        } else if (previous_op == OP_OR && previous_result == 0) {
+            return EXECUTION_SUCCESS;
+        }
+
+        if (strcmp(cmd->args[0], "exit") == 0) return EXECUTION_REQUEST_EXIT; // Exit command
+
+        status = execvp(cmd->args[0], cmd->args); // returns -1 if the command failed
+        if (status == EXECUTION_FAILED) {
             printf("%s: command not found\n", cmd->args[0]);
         }
     } else { // parent process
@@ -54,15 +61,16 @@ int execute_command(const struct command *cmd, enum op previous_op) {
  */
 int sh_run(struct command *cmd) {
     if (!cmd || cmd->args[0] == NULL) return EXECUTION_FAILED; // Empty command
-    if (strcmp(cmd->args[0], "exit") == 0) return EXECUTION_REQUEST_EXIT; // Exit command
 
     // Initialize the previous operator
     enum op previous_op = (enum op) NULL;
+    int previous_result = 0;
 
     // Execute the commands
     struct command *current = cmd;
     while (current != NULL) {
-        execute_command(current, previous_op);
+        previous_result = execute_command(current, previous_op, previous_result);
+        if (previous_result == EXECUTION_REQUEST_EXIT) break;
         previous_op = current->op;
         current = current->next;
     }
