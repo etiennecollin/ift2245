172293adb4b16ca136d5bca2a882fd00ed572a9f
grosjuice <villeneuve_99@hotmail.com>
2024-03-31 14:36:48 -0400

    vmm write implemented

--- src/vmm.c
+++ src/vmm.c
@@ -43,7 +43,6 @@ char vmm_read (unsigned int laddress)
   read_count++;
 
   unsigned int temp = laddress;
-
   unsigned int page_offset = temp & 0xFF; // get first 8 bits of the address
   unsigned int page_number = (temp >> 8) & 0xFF; // get the last 8 bits of the address
 
@@ -58,9 +57,9 @@ char vmm_read (unsigned int laddress)
 
   // build physical address
   unsigned int physical_address = frame;
-  physical_address = (physical_address << 8);
+  physical_address = physical_address << 8;
   physical_address = physical_address | page_offset;
-  
+
   vmm_log_command (stdout, "READING", laddress, page_number, frame, page_offset, physical_address, c);
   return c;
 }
@@ -69,10 +68,26 @@ char vmm_read (unsigned int laddress)
 void vmm_write (unsigned int laddress, char c)
 {
   write_count++;
-  /* ¡ TODO: COMPLÉTER ! */
+  unsigned int temp = laddress;
+  unsigned int page_offset = temp & 0xFF; // get first 8 bits of the address
+  unsigned int page_number = (temp >> 8) & 0xFF; // get the last 8 bits of the address
 
-  // TODO: Fournir les arguments manquants.
-  vmm_log_command (stdout, "WRITING", laddress, 0, 0, 0, 0, c);
+  // check if the page is in the TLB
+  int frame = tlb_lookup(page_number, false);
+  if (frame == -1) { // page is not in the TLB. Check in page table.
+      // frame = pt_lookup(page_number);
+      frame = pt_lookup(page_number);
+      if (frame == -1) { // frame number is not valid TODO: handle this case
+
+        }
+    }
+
+    // build physical address
+    unsigned int physical_address = frame;
+    physical_address = physical_address << 8;
+    physical_address = physical_address | page_offset;
+
+  vmm_log_command (stdout, "WRITING", laddress, page_number, frame, page_offset, physical_address, c);
 }
 
 
