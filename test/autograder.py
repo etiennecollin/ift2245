19b06cc310c0b5fba110692fca1f7d6f5899544e
Jay-D13 <jaydaninparis@gmail.com>
2024-05-07 16:55:53 -0400

    Received a forced update  from Github Classroom

--- test/autograder.py
+++ test/autograder.py
@@ -2,6 +2,8 @@ import os
 import subprocess
 import re
 
+pattern = re.compile(r'Score:\s*([\d.]+)')
+
 # Print autograder header
 
 print("")
@@ -25,11 +27,20 @@ except subprocess.TimeoutExpired:
 
 scheduler_result = None
 try:
-    scheduler_result = subprocess.run(['../src/scheduler', 'test_quick.csv'], stdout=subprocess.PIPE, text=True,
-                                      timeout=30)
+    scheduler_result = subprocess.run(['../src/scheduler', 'test_quick.csv'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True,
+                                      timeout=120)
 except subprocess.TimeoutExpired:
     scheduler_result = None
+    
+# if the small passes, let's run the big fella
+big_scheduler_result = None
+try:
+    big_scheduler_result = subprocess.run(['../src/scheduler', 'test_grader.csv'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True,
+                                        timeout=500)
+except subprocess.TimeoutExpired:
+    big_scheduler_result = None
 
+q_passed = 0
 if ready_queue_result is not None:
     result = re.search(r'Checks: (\d+), Failures: (\d+), Errors: (\d+)', ready_queue_result.stdout)
 
@@ -37,11 +48,31 @@ if ready_queue_result is not None:
     failures = int(result.group(2))
     errors = int(result.group(3))
 
+    q_passed = checks - failures - errors
     print(f"Ready Queue Tests: {checks - failures - errors}/{checks}")
 else:
     print(f"Ready Queue Tests: 0/8 (timeout)")
 
-if scheduler_result is not None:
+subgrade = (30 / 8) * q_passed
+
+if scheduler_result is not None and scheduler_result.returncode == 0:
     print(f"Scheduler: passed")
+    subgrade += 20
+elif scheduler_result is not None and scheduler_result.returncode != 0:
+    print(f"Scheduler: {scheduler_result.stdout}")
 else:
-    print(f"Scheduler: failed (timeout)")
+    print(f"Scheduler: timeout")
+
+print(f"Subgrade: {subgrade}")
+
+if big_scheduler_result is not None and big_scheduler_result.returncode == 0:
+    score = pattern.search(big_scheduler_result.stdout)
+    if score is not None:
+        score = float(score.group(1))
+    else:
+        print(f"Big Scheduler: {big_scheduler_result.stdout} | Score: 999")
+    print(f"Big Scheduler: passed | Score: {score}")
+elif big_scheduler_result is not None and big_scheduler_result.returncode != 0:
+    print(f"Big Scheduler: {big_scheduler_result.stdout} | Score: 999")
+else:
+    print(f"Big Scheduler: timeout | Score: 999")
